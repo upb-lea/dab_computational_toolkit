7ea190dbc16a22d30f2792e9f2aaea48dbd15efe
gituser789 <62549000+gituser789@users.noreply.github.com>
2024-06-21 17:19:32 +0200

    add unit tests to github runner

--- examples/dto_change.py
+++ examples/dto_change.py
@@ -1,16 +1,12 @@
+"""Show how to work with DTOs as object structure."""
 import dct
 
-
-
-#
 loaded_dto = dct.load_dab_specification('initial')
 loaded_dto = dct.HandleDabDto.add_gecko_simulation_results(loaded_dto)
 dct.HandleDabDto.save(loaded_dto, name='initial_with_simulation_results', comment='', directory=None, timestamp=False)
 
+# loaded_dto = dct.HandleDabDto.load_from_file('initial_with_simulation_results.npz')
 
-#loaded_dto = dct.HandleDabDto.load_from_file('initial_with_simulation_results.npz')
-
-#dct.plot_gecko_simulation_results(loaded_dto, simulation_name='döner', comment='comment', directory=None, show_plot=True)
-
+# dct.plot_gecko_simulation_results(loaded_dto, simulation_name='döner', comment='comment', directory=None, show_plot=True)
 
 # dct.plot_calculation_results(loaded_dto)
