fix error in alpha_rad angle calculation

dct/currents.py
@@ -6,7 +6,7 @@ from dct.datasets import Config, CalcFromConfig, CalcModulation
 # 3rd party libraries
 import numpy as np
 
-def calc_l_s_mode_2_currents(phi_rad, tau_1_rad, tau_2_rad, v_1, d, f_s, l_s):
+def _calc_l_s_mode_2_currents(phi_rad, tau_1_rad, tau_2_rad, v_1, d, f_s, l_s):
     """
     Calculate currents in l_s for the given angles alpha_rad, beta_rad, gamma_rad and delta_rad.
 
@@ -31,7 +31,7 @@ def calc_l_s_mode_2_currents(phi_rad, tau_1_rad, tau_2_rad, v_1, d, f_s, l_s):
 
     return i_l_s_alpha, i_l_s_beta, i_l_s_gamma, i_l_s_delta
 
-def calc_l_1_mode_2_currents(phi_rad, tau_1_rad, tau_2_rad, v_1, f_s, l_1):
+def _calc_l_1_mode_2_currents(phi_rad, tau_1_rad, tau_2_rad, v_1, f_s, l_1):
     """
     Calculate currents in l_1 for the given angles alpha_rad, beta_rad, gamma_rad and delta_rad.
 
@@ -55,7 +55,7 @@ def calc_l_1_mode_2_currents(phi_rad, tau_1_rad, tau_2_rad, v_1, f_s, l_1):
 
     return i_l_1_alpha, i_l_1_beta, i_l_1_gamma, i_l_1_delta
 
-def calc_l_2_mode_2_currents(tau_2_rad, v_2, f_s, l_2):
+def _calc_l_2_mode_2_currents(tau_2_rad, v_2, f_s, l_2):
     """
     Calculate currents in l_2 for the given angles alpha_rad, beta_rad, gamma_rad and delta_rad.
 
@@ -81,7 +81,7 @@ def calc_l_2_mode_2_currents(tau_2_rad, v_2, f_s, l_2):
 
     return i_l_s_alpha, i_l_2_beta, i_l_2_gamma, i_l_2_delta
 
-def calc_l_s_mode_1_plus_currents(phi_rad, tau_1_rad, tau_2_rad, d, v_1, f_s, l_s):
+def _calc_l_s_mode_1_plus_currents(phi_rad, tau_1_rad, tau_2_rad, d, v_1, f_s, l_s):
     """
     Calculate currents in l_s for the given angles alpha_rad, beta_rad, gamma_rad and delta_rad.
 
@@ -106,7 +106,7 @@ def calc_l_s_mode_1_plus_currents(phi_rad, tau_1_rad, tau_2_rad, d, v_1, f_s, l_
 
     return i_l_s_alpha_rad, i_l_s_beta_rad, i_l_s_gamma_rad, i_l_s_delta_rad
 
-def calc_l_1_mode_1_plus_currents(phi_rad, tau_1_rad, tau_2_rad, v_1, f_s, l_1):
+def _calc_l_1_mode_1_plus_currents(phi_rad, tau_1_rad, tau_2_rad, v_1, f_s, l_1):
     """
     Calculate currents in l_1 for the given angles alpha_rad, beta_rad, gamma_rad and delta_rad.
 
@@ -130,7 +130,7 @@ def calc_l_1_mode_1_plus_currents(phi_rad, tau_1_rad, tau_2_rad, v_1, f_s, l_1):
 
     return i_l_1_alpha, i_l_1_beta, i_l_1_gamma, i_l_1_delta
 
-def calc_l_2_mode_1_plus_currents(phi_rad, tau_1_rad, tau_2_rad, v_2, f_s, l_2):
+def _calc_l_2_mode_1_plus_currents(phi_rad, tau_1_rad, tau_2_rad, v_2, f_s, l_2):
     """
     Calculate currents in l_2 for the given angles alpha_rad, beta_rad, gamma_rad and delta_rad.
 
@@ -158,7 +158,7 @@ def calc_l_2_mode_1_plus_currents(phi_rad, tau_1_rad, tau_2_rad, v_2, f_s, l_2):
 
     return i_l_2_alpha, i_l_2_beta, i_l_2_gamma, i_l_2_delta
 
-def int_square_line_between_angles(angle_1_rad, angle_2_rad, y_1, y_2):
+def _int_square_line_between_angles(angle_1_rad, angle_2_rad, y_1, y_2):
     """
     Calculate the integral of the square for a simple line, described with two points.
 
@@ -217,16 +217,21 @@ def calc_rms(alpha_rad, beta_rad, gamma_rad, delta_rad, i_alpha, i_beta, i_gamma
     currents_vec[angles_vec < 0] = -currents_vec[angles_vec < 0]
     angles_vec[angles_vec < 0] = angles_vec[angles_vec < 0] + np.pi
 
+    # again: if angles still below zero:
+    # if angle < 0, add pi and invert current. Current first, as angle will be always > 0 after modification
+    currents_vec[angles_vec < 0] = -currents_vec[angles_vec < 0]
+    angles_vec[angles_vec < 0] = angles_vec[angles_vec < 0] + np.pi
+
     # sort the angles and currents according to the angle order.
     # https://stackoverflow.com/questions/52121635/looking-up-index-of-value-in-numpy-3d-arrays
     sorted_indices = np.argsort(angles_vec, axis=0)
     angles_sorted = np.take_along_axis(angles_vec, sorted_indices, axis=0)
     currents_sorted = np.take_along_axis(currents_vec, sorted_indices, axis=0)
 
-    int_square_part_00 = int_square_line_between_angles(angles_zeros, angles_sorted[0], -currents_sorted[3], currents_sorted[0])
-    int_square_part_01 = int_square_line_between_angles(angles_sorted[0], angles_sorted[1], currents_sorted[0], currents_sorted[1])
-    int_square_part_12 = int_square_line_between_angles(angles_sorted[1], angles_sorted[2], currents_sorted[1], currents_sorted[2])
-    int_square_part_23 = int_square_line_between_angles(angles_sorted[2], angles_sorted[3], currents_sorted[2], currents_sorted[3])
+    int_square_part_00 = _int_square_line_between_angles(angles_zeros, angles_sorted[0], -currents_sorted[3], currents_sorted[0])
+    int_square_part_01 = _int_square_line_between_angles(angles_sorted[0], angles_sorted[1], currents_sorted[0], currents_sorted[1])
+    int_square_part_12 = _int_square_line_between_angles(angles_sorted[1], angles_sorted[2], currents_sorted[1], currents_sorted[2])
+    int_square_part_23 = _int_square_line_between_angles(angles_sorted[2], angles_sorted[3], currents_sorted[2], currents_sorted[3])
 
     rms = np.sqrt(2 * (int_square_part_00 + int_square_part_01 + int_square_part_12 + int_square_part_23))
 
@@ -242,7 +247,7 @@ def calc_rms_currents(config: Config, calc_from_config: CalcFromConfig, calc_mod
     :param config: design configuration DTO
     :return: i_l_s_rms, i_l_1_rms, i_l_2_rms
     """
-    alpha_rad = calc_modulation.phi - calc_modulation.tau1
+    alpha_rad = np.pi - calc_modulation.tau1
     beta_rad = np.pi + calc_modulation.phi - calc_modulation.tau2
     gamma_rad = np.full_like(alpha_rad, np.pi)
     delta_rad = np.pi + calc_modulation.phi
@@ -254,21 +259,21 @@ def calc_rms_currents(config: Config, calc_from_config: CalcFromConfig, calc_mod
     d = config.n * calc_from_config.mesh_V2 / calc_from_config.mesh_V1
 
     # currents in l_s for mode 2 and mode 1+
-    m2_i_l_s_alpha, m2_i_l_s_beta, m2_i_l_s_gamma, m2_i_l_s_delta = calc_l_s_mode_2_currents(
+    m2_i_l_s_alpha, m2_i_l_s_beta, m2_i_l_s_gamma, m2_i_l_s_delta = _calc_l_s_mode_2_currents(
         calc_modulation.phi, calc_modulation.tau1, calc_modulation.tau2, calc_from_config.mesh_V1, d, config.fs, config.Ls)
-    m1_i_l_s_alpha, m1_i_l_s_beta, m1_i_l_s_gamma, m1_i_l_s_delta = calc_l_s_mode_1_plus_currents(
+    m1_i_l_s_alpha, m1_i_l_s_beta, m1_i_l_s_gamma, m1_i_l_s_delta = _calc_l_s_mode_1_plus_currents(
         calc_modulation.phi, calc_modulation.tau1, calc_modulation.tau2, d, calc_from_config.mesh_V1, config.fs, config.Ls)
 
     # currents in l_1 for mode 2 and mode 1+
-    m2_i_l_1_alpha, m2_i_l_1_beta, m2_i_l_1_gamma, m2_i_l_1_delta = calc_l_1_mode_2_currents(
+    m2_i_l_1_alpha, m2_i_l_1_beta, m2_i_l_1_gamma, m2_i_l_1_delta = _calc_l_1_mode_2_currents(
         calc_modulation.phi, calc_modulation.tau1, calc_modulation.tau2, calc_from_config.mesh_V1, config.fs, config.Lc1)
-    m1_i_l_1_alpha, m1_i_l_1_beta, m1_i_l_1_gamma, m1_i_l_1_delta = calc_l_1_mode_1_plus_currents(
+    m1_i_l_1_alpha, m1_i_l_1_beta, m1_i_l_1_gamma, m1_i_l_1_delta = _calc_l_1_mode_1_plus_currents(
         calc_modulation.phi, calc_modulation.tau1, calc_modulation.tau2, calc_from_config.mesh_V1, config.fs, config.Lc1)
 
     # currents in l_2 for mode 2 and mode 1+
-    m2_i_l_2_alpha, m2_i_l_2_beta, m2_i_l_2_gamma, m2_i_l_2_delta = calc_l_2_mode_2_currents(
+    m2_i_l_2_alpha, m2_i_l_2_beta, m2_i_l_2_gamma, m2_i_l_2_delta = _calc_l_2_mode_2_currents(
         calc_modulation.tau2, calc_from_config.mesh_V2, config.fs, config.Lc2)
-    m1_i_l_2_alpha, m1_i_l_2_beta, m1_i_l_2_gamma, m1_i_l_2_delta = calc_l_2_mode_1_plus_currents(
+    m1_i_l_2_alpha, m1_i_l_2_beta, m1_i_l_2_gamma, m1_i_l_2_delta = _calc_l_2_mode_1_plus_currents(
         calc_modulation.phi, calc_modulation.tau1, calc_modulation.tau2, calc_from_config.mesh_V2, config.fs, config.Lc2)
 
     # generate the output current for l_s, distinguish between mode 2 and mode 1+

dct/plot_waveforms.py
@@ -20,23 +20,36 @@ def plot_calc_waveforms(dab_dto: DabDTO):
 
         sorted_total_angles = np.append(sorted_angles, np.pi + sorted_angles)
         sorted_i_l_s_total = np.append(i_l_s_sorted, -1 * i_l_s_sorted)
+        sorted_i_l_1_total = np.append(i_l_1_sorted, -1 * i_l_1_sorted)
+        sorted_i_l_2_total = np.append(i_l_2_sorted, -1 * i_l_2_sorted)
         sorted_total_angles = np.append(np.array([0]), sorted_total_angles)
         sorted_i_l_s_total = np.append(sorted_i_l_s_total[-1], sorted_i_l_s_total)
+        sorted_i_l_1_total = np.append(sorted_i_l_1_total[-1], sorted_i_l_1_total)
+        sorted_i_l_2_total = np.append(sorted_i_l_2_total[-1], sorted_i_l_2_total)
 
-        print(f"{sorted_angles=}")
-        print(f"{i_l_s_sorted=}")
+        # plot arrays with elements only (neglect nan-arrays)
+        if np.all(~np.isnan(sorted_total_angles)):
 
-        plt.subplot(311)
-        plt.plot(sorted_total_angles, sorted_i_l_s_total)
-        plt.ylabel('i_L_s in A')
+            print(f"{sorted_angles=}")
+            print(f"{i_l_s_sorted=}")
 
-        plt.subplot(312)
-        plt.plot(sorted_angles, i_l_1_sorted)
-        plt.ylabel('i_L_1 in A')
+            plt.subplot(311)
+            plt.plot(sorted_total_angles, sorted_i_l_s_total)
+            plt.ylabel('i_L_s in A')
+            if dab_dto.calc_modulation.mask_IIIm1[vec_vvp]:
+                plt.title(f"{dab_dto.calc_modulation.mask_IIIm1[vec_vvp]=}")
+            if dab_dto.calc_modulation.mask_IIm2[vec_vvp]:
+                plt.title(f"{dab_dto.calc_modulation.mask_IIm2[vec_vvp]=}")
+            if dab_dto.calc_modulation.mask_Im2[vec_vvp]:
+                plt.title(f"{dab_dto.calc_modulation.mask_Im2[vec_vvp]=}")
 
-        plt.subplot(313)
-        plt.plot(sorted_angles, i_l_2_sorted)
-        plt.ylabel('i_L_2 in A')
+            plt.subplot(312)
+            plt.plot(sorted_total_angles, sorted_i_l_1_total)
+            plt.ylabel('i_L_1 in A')
 
-        plt.tight_layout()
-        plt.show()
+            plt.subplot(313)
+            plt.plot(sorted_total_angles, sorted_i_l_2_total)
+            plt.ylabel('i_L_2 in A')
+
+            plt.tight_layout()
+            plt.show()

tests/currents_test.py
@@ -24,9 +24,30 @@ def test_rms_1d():
     i_gamma = np.array([1.0])
     i_delta = np.array([0.0])
 
-    i_rms = dct.calc_rms(alpha_rad, beta_rad, gamma_rad, delta_rad, i_alpha, i_beta, i_gamma, i_delta)
+    i_rms, _, _ = dct.calc_rms(alpha_rad, beta_rad, gamma_rad, delta_rad, i_alpha, i_beta, i_gamma, i_delta)
 
     assert i_rms == approx(1.364, rel=5e-3)
+
+def test_rms_1d_sorting_out_of_pi_range():
+    """
+    Test RMS current calculation with a 1D-array.
+
+    All results have been verified using GeckoCIRCUITS.
+    """
+    alpha_rad = np.array([-1])  # -1 + 3.14 = 2.14 (number3)
+    beta_rad = np.array([-2.0])  # -2 + 3.14 = 1.14 (number 2)
+    gamma_rad = np.array([4.0])  # 4-3.14 = 0.86 (number 1)
+    delta_rad = np.array([np.pi])  # number 4
+
+    i_alpha = np.array([-2.0])  # Number 3, positive due to shifting
+    i_beta = np.array([3.0])  # Number 2, negative due to shifting
+    i_gamma = np.array([1.0])  # Number 1, negative due to shifting
+    i_delta = np.array([5.0])  # Number 4, no shifting, positive
+
+    i_rms, angles_sorted, currents_sorted = dct.calc_rms(alpha_rad, beta_rad, gamma_rad, delta_rad, i_alpha, i_beta, i_gamma, i_delta)
+
+    assert angles_sorted == approx(np.array([[0.85840735], [1.14159265], [2.14159265], [3.14159265]]), rel=5e-3)
+    assert currents_sorted == approx(np.array([[-1.], [-3.], [2.], [5.]]))
 
 def test_rms_2d():
     """
@@ -49,7 +70,7 @@ def test_rms_2d():
     i_gamma = np.array([[1.0, 0.0], [1.0, -1.0]])
     i_delta = np.array([[0.0, 1.0], [-2.0, -2.0]])
 
-    i_rms = dct.calc_rms(alpha_rad, beta_rad, gamma_rad, delta_rad, i_alpha, i_beta, i_gamma, i_delta)
+    i_rms, _, _ = dct.calc_rms(alpha_rad, beta_rad, gamma_rad, delta_rad, i_alpha, i_beta, i_gamma, i_delta)
 
     assert i_rms == approx(np.array([[1.364, 1.364], [2.08, 1.94]]), rel=5e-3)
 
@@ -69,6 +90,6 @@ def test_rms_3d():
     i_gamma = np.array([[[1.0, 0.0], [1.0, -1.0]], [[1.0, 0.0], [1.0, -1.0]]])
     i_delta = np.array([[[0.0, 1.0], [-2.0, -2.0]], [[0.0, 1.0], [-2.0, -2.0]]])
 
-    i_rms = dct.calc_rms(alpha_rad, beta_rad, gamma_rad, delta_rad, i_alpha, i_beta, i_gamma, i_delta)
+    i_rms, _, _ = dct.calc_rms(alpha_rad, beta_rad, gamma_rad, delta_rad, i_alpha, i_beta, i_gamma, i_delta)
 
     assert i_rms == approx(np.array([[[1.364, 1.364], [2.08, 1.94]], [[1.364, 1.364], [2.08, 1.94]]]), rel=5e-3)