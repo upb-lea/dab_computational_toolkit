fix significant error in l_s mode 2 current calculation from literature

dct/datasets.py
@@ -122,6 +122,7 @@ class CalcCurrents:
 
     # sorted values: angles (alpha, beta, gamma, delta) and currents.
     angles_rad_sorted: np.array
+    angles_rad_unsorted: np.array
     i_l_s_sorted: np.array
     i_l_1_sorted: np.array
     i_l_2_sorted: np.array
@@ -285,15 +286,16 @@ class HandleDabDto:
         calc_config = HandleDabDto.calculate_from_configuration(config=input_configuration)
         modulation_parameters = HandleDabDto.calculate_modulation(input_configuration, calc_config)
 
-        i_l_s_rms, i_l_1_rms, i_l_2_rms, angles_rad_sorted, i_l_s_sorted, i_l_1_sorted, i_l_2_sorted = dct.calc_rms_currents(
+        i_l_s_rms, i_l_1_rms, i_l_2_rms, angles_rad_sorted, i_l_s_sorted, i_l_1_sorted, i_l_2_sorted, angles_rad_unsorted = dct.calc_rms_currents(
             input_configuration, calc_config, modulation_parameters)
 
         calc_currents = CalcCurrents(**{'i_l_s_rms': i_l_s_rms, 'i_l_1_rms': i_l_1_rms, 'i_l_2_rms': i_l_2_rms, 'angles_rad_sorted': angles_rad_sorted,
+                                        'angles_rad_unsorted': angles_rad_unsorted,
                                         'i_l_s_sorted': i_l_s_sorted, 'i_l_1_sorted': i_l_1_sorted, 'i_l_2_sorted': i_l_2_sorted})
 
         gecko_additional_params = GeckoAdditionalParameters(
             t_dead1=50e-9, t_dead2=50e-9, timestep=1e-9,
-            simtime=50e-6, timestep_pre=50e-9, simtime_pre=5e-3,
+            simtime=50e-6, timestep_pre=25e-9, simtime_pre=5e-3,
             simfilepath=os.path.abspath(os.path.join(os.path.abspath(__file__), '..', '..', 'circuits', 'DAB_MOSFET_Modulation_v8.ipes')),
             lossfilepath=os.path.abspath(os.path.join(os.path.abspath(__file__), '..', '..', 'circuits')))
 

dct/plot_waveforms.py
@@ -17,6 +17,7 @@ def plot_calc_waveforms(dab_dto: DabDTO, compare_gecko_waveforms: bool = False):
 
         # set simulation parameters and convert tau to degree for Gecko
         sorted_angles = np.transpose(dab_dto.calc_currents.angles_rad_sorted, (1, 2, 3, 0))[vec_vvp]
+        unsorted_angles = np.transpose(dab_dto.calc_currents.angles_rad_unsorted, (1, 2, 3, 0))[vec_vvp]
         i_l_s_sorted = np.transpose(dab_dto.calc_currents.i_l_s_sorted, (1, 2, 3, 0))[vec_vvp]
         i_l_1_sorted = np.transpose(dab_dto.calc_currents.i_l_1_sorted, (1, 2, 3, 0))[vec_vvp]
         i_l_2_sorted = np.transpose(dab_dto.calc_currents.i_l_2_sorted, (1, 2, 3, 0))[vec_vvp]
@@ -44,12 +45,14 @@ def plot_calc_waveforms(dab_dto: DabDTO, compare_gecko_waveforms: bool = False):
             plt.ylabel('i_L_s in A')
             plt.grid()
             plt.legend()
+            plot_info = f", P= {dab_dto.calc_config.mesh_P[vec_vvp]}W, angles= {unsorted_angles}, currents= {sorted_i_l_s_total}, v1={dab_dto.calc_config.mesh_V1[vec_vvp]}, v2={dab_dto.calc_config.mesh_V2[vec_vvp]}, tau2={dab_dto.calc_modulation.tau2[vec_vvp]}"
+
             if dab_dto.calc_modulation.mask_IIIm1[vec_vvp]:
-                plt.title(f"IIIm1, P= {dab_dto.calc_config.mesh_P[vec_vvp]}W")
+                plt.title(f"IIIm1" + plot_info)
             if dab_dto.calc_modulation.mask_IIm2[vec_vvp]:
-                plt.title(f"IIm2, P= {dab_dto.calc_config.mesh_P[vec_vvp]} W")
+                plt.title(f"IIm2" + plot_info)
             if dab_dto.calc_modulation.mask_Im2[vec_vvp]:
-                plt.title(f"Im2, P= {dab_dto.calc_config.mesh_P[vec_vvp]} W")
+                plt.title(f"Im2" + plot_info)
 
             plt.subplot(312, sharex=ax1)
             plt.plot(sorted_total_angles, sorted_i_l_1_total, label='calculation')