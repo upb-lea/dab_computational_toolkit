3bfdcb4a7cc74213f0f76479cecbf3f4725f9bd4
gituser789 <62549000+gituser789@users.noreply.github.com>
2024-09-27 12:10:55 +0200

    add check for loaded DTOs

--- examples/pareto_heat_sink.py
+++ examples/pareto_heat_sink.py
@@ -30,6 +30,10 @@ circuit_numbers = [entry.name.split(os.extsep)[0] for entry in circuit_objects]
 for circuit_number in circuit_numbers:
     circuit_filepath_results = os.path.join(filepaths.circuit, circuit_study_name, "filtered_results")
     circuit_filepath_number = os.path.join(circuit_filepath_results, f"{circuit_number}.pkl")
+
+    # Get circuit results
+    circuit_dto_results = paretodab.HandleDabDto.load_from_file(circuit_filepath_number)
+
     # iterate inductor study
     for inductor_study_name in inductor_study_name_list:
         inductor_filepath_results = os.path.join(filepaths.inductor, circuit_study_name, circuit_number,
@@ -38,11 +42,21 @@ for circuit_number in circuit_numbers:
 
             inductor_objects = os.scandir(inductor_filepath_results)
             inductor_numbers = [entry.name.split(os.extsep)[0] for entry in inductor_objects]
-            print(inductor_numbers)
 
             # iterate inductor numbers
             for inductor_number in inductor_numbers:
                 inductor_filepath_number = os.path.join(inductor_filepath_results, f"{inductor_number}.pkl")
+
+                # Get inductor results
+                with open(inductor_filepath_number, 'rb') as pickle_file_data:
+                    inductance_dto_results = pickle.load(pickle_file_data)
+
+                if int(inductance_dto_results.circuit_trial_number) != int(circuit_number):
+                    raise ValueError(f"{inductance_dto_results.circuit_trial_number=} != {circuit_number}")
+                if int(inductance_dto_results.inductor_trial_number) != int(inductor_number):
+                    raise ValueError(f"{inductance_dto_results.inductor_trial_number=} != {inductor_number}")
+
+                inductance_loss_matrix = inductance_dto_results.p_combined_losses
 
                 for stacked_transformer_study_name in stacked_transformer_study_name_list:
                     stacked_transformer_filepath_results = os.path.join(filepaths.transformer, circuit_study_name, circuit_number,
@@ -53,13 +67,17 @@ for circuit_number in circuit_numbers:
 
                     for stacked_transformer_number in stacked_transformer_numbers:
                         stacked_transformer_filepath_number = os.path.join(stacked_transformer_filepath_results, f"{stacked_transformer_number}.pkl")
-                        # Get inductor results
-                        with open(inductor_filepath_number, 'rb') as pickle_file_data:
-                            inductance_dto_results = pickle.load(pickle_file_data)
-                        inductance_loss_matrix = inductance_dto_results.p_combined_losses
 
-                        # Get circuit results
-                        circuit_dto_results = paretodab.HandleDabDto.load_from_file(circuit_filepath_number)
+                        # get transformer results
+                        with open(stacked_transformer_filepath_number, 'rb') as pickle_file_data:
+                            transformer_dto_results = pickle.load(pickle_file_data)
+
+                        if int(transformer_dto_results.circuit_trial_number) != int(circuit_number):
+                            raise ValueError(f"{transformer_dto_results.circuit_trial_number=} != {circuit_number}")
+                        if int(transformer_dto_results.stacked_transformer_trial_number) != int(stacked_transformer_number):
+                            raise ValueError(f"{transformer_dto_results.stacked_transformer_trial_number=} != {stacked_transformer_number}")
+
+                        transformer_loss_matrix = transformer_dto_results.p_combined_losses
 
                         # get transistor results
                         total_transistor_cond_loss_matrix = 2 * (circuit_dto_results.gecko_results.S11_p_cond + circuit_dto_results.gecko_results.S12_p_cond + \
@@ -76,11 +94,6 @@ for circuit_number in circuit_numbers:
                             np.greater(circuit_dto_results.gecko_results.S24_p_cond, circuit_dto_results.gecko_results.S23_p_cond)] = \
                             circuit_dto_results.gecko_results.S24_p_cond[
                             np.greater(circuit_dto_results.gecko_results.S24_p_cond, circuit_dto_results.gecko_results.S23_p_cond)]
-
-                        # get transformer results
-                        with open(stacked_transformer_filepath_number, 'rb') as pickle_file_data:
-                            transformer_dto_results = pickle.load(pickle_file_data)
-                        transformer_loss_matrix = transformer_dto_results.p_combined_losses
 
                         total_loss_matrix = (inductance_dto_results.p_combined_losses + total_transistor_cond_loss_matrix + \
                                              transformer_dto_results.p_combined_losses)
@@ -129,7 +142,6 @@ for circuit_number in circuit_numbers:
                             "transformer_t_max": 0,
                         }
                         local_df = pd.DataFrame([data])
-                        print(local_df.head())
 
                         global_df = pd.concat([global_df, local_df], axis=0)
 
