c0a125323f4ef91488681f806be788be4efff940
gituser789 <62549000+gituser789@users.noreply.github.com>
2024-10-01 09:51:00 +0200

    add HCT pareto optimization for heat sink

--- examples/pareto_heat_sink.py
+++ examples/pareto_heat_sink.py
@@ -10,6 +10,7 @@ from matplotlib import pyplot as plt
 
 # own libraries
 import paretodab
+import hct
 
 # specify input parameters
 project_name = "2024-09-12_project_dab_paper"
@@ -204,8 +205,6 @@ for circuit_number in circuit_numbers:
 
                         df = pd.concat([df, local_df], axis=0)
 
-
-df["total_volume"] = df["transformer_volume"] + df["inductor_volume"]
 df["total_area"] = df["circuit_area"] + df["inductor_area"] + df["transformer_area"]
 df["total_mean_loss"] = df["circuit_mean_loss"] + df["inductor_mean_loss"] + df["transformer_mean_loss"]
 
@@ -213,11 +212,33 @@ df["t_heat_sink_lowest"] = df[["circuit_heat_sink_max_1", "circuit_heat_sink_max
 df["t_heat_sink_dimensioning"] = df["t_heat_sink_lowest"].apply(lambda x: x if x < heat_sink.t_hs_max else heat_sink.t_hs_max)
 df["r_th_heat_sink"] = (df["t_heat_sink_dimensioning"] - heat_sink.t_ambient) / df["total_losses"]
 
+for (_, _, file_name_list) in os.walk('../../HCT_heat_sink_computation_toolbox/hct/data'):
+    fan_list = file_name_list
+
+config = hct.OptimizationParameters(
+    heat_sink_study_name="heat_sink_trial_1",
+    heat_sink_optimization_directory=filepaths.heat_sink,
+    height_c_list=[0.02, 0.08],
+    width_b_list=[0.02, 0.08],
+    length_l_list=[0.08, 0.20],
+    height_d_list=[0.001, 0.003],
+    number_fins_n_list=[5, 20],
+    thickness_fin_t_list=[1e-3, 5e-3],
+    fan_list=fan_list,
+    t_ambient=40,
+)
+
+# hct.Optimization.start_proceed_study(config=config, number_trials=10000)
+
+df_heat_sink = hct.Optimization.study_to_df(config)
+# hct.Optimization.df_plot_pareto_front(df_heat_sink, (50, 60))
+
+df["heat_sink_volume"] = df["r_th_heat_sink"].apply(lambda r_th_max: df_heat_sink.loc[df_heat_sink["values_1"] < r_th_max]["values_0"].nsmallest(n=1))
+
+df["total_volume"] = df["transformer_volume"] + df["inductor_volume"] + df["heat_sink_volume"]
 
 plt.scatter(df["total_volume"], df["total_mean_loss"])
 plt.grid()
 plt.xlabel("Volume in m³")
 plt.ylabel("Mean total losses in W")
 plt.show()
-
-print(df.head())
