e93f17d7ea94ec115d426ae0df59fa5d9857be0d
gituser789 <62549000+gituser789@users.noreply.github.com>
2025-01-31 16:56:49 +0100

    adapt heat sink example

--- examples/pareto_heat_sink.py
+++ examples/pareto_heat_sink.py
@@ -12,12 +12,7 @@ import dct
 
 
 # specify input parameters
-project_name = "2024-10-04_dab_paper"
-circuit_study_name = "circuit_paper_trial_1"
-inductor_study_name_list = ["inductor_trial_1"]
-stacked_transformer_study_name_list = ["transformer_trial_1"]
-
-df = pd.DataFrame()
+project_name = "2025-01-31_example"
 
 # load project file paths
 filepaths = dct.Optimization.load_filepaths(os.path.abspath(os.path.join(os.curdir, project_name)))
@@ -26,7 +21,7 @@ for (_, _, file_name_list) in os.walk('../../HCT_heat_sink_computation_toolbox/h
     fan_list = file_name_list
 
 config = hct.OptimizationParameters(
-    heat_sink_study_name="heat_sink_trial_3_dimensions",
+    heat_sink_study_name="3_dimensions",
     heat_sink_optimization_directory=filepaths.heat_sink,
     height_c_list=[0.02, 0.08],
     width_b_list=[0.02, 0.08],
@@ -40,10 +35,10 @@ config = hct.OptimizationParameters(
     number_directions=3
 )
 
-hct.Optimization.start_proceed_study(config=config, number_trials=30000)
+hct.Optimization.start_proceed_study(config=config, number_trials=1000)
 
 df_heat_sink = hct.Optimization.study_to_df(config)
 hct.Optimization.df_plot_pareto_front(df_heat_sink, (50, 60))
 
-df["heat_sink_volume"] = df["r_th_heat_sink"].apply(lambda r_th_max: df_heat_sink.loc[df_heat_sink["values_1"] < r_th_max]["values_0"].nsmallest(n=1))
-df["total_volume"] = df["transformer_volume"] + df["inductor_volume"] + df["heat_sink_volume"]
+# df_heat_sink["heat_sink_volume"] = df_heat_sink["r_th_heat_sink"].apply(lambda r_th_max: df_heat_sink.loc[df_heat_sink["values_1"] < r_th_max]["values_0"].nsmallest(n=1))
+# df_heat_sink["total_volume"] = df_heat_sink["transformer_volume"] + df_heat_sink["inductor_volume"] + df_heat_sink["heat_sink_volume"]
