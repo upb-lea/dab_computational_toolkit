bd92bca98f9ef126c7083ba80b001acd8e2c73fd
gituser789 <62549000+gituser789@users.noreply.github.com>
2025-03-07 13:37:58 +0100

    add transistor loss function

---
+++ dct/losses.py
@@ -0,0 +1,20 @@
+"""Calculate the transistor losses."""
+
+# 3rd party libraries
+import numpy as np
+
+# own modules
+import dct.datasets_dtos as dtos
+
+def transistor_conduction_loss(transistor_rms_current: float, transistor_dto: dtos.TransistorDTO) -> float:
+    """
+    Calculate the transistor conduction losses.
+
+    :param transistor_rms_current: transistor RMS current in A
+    :type transistor_rms_current: float
+    :param transistor_dto: transistor DTO (Data transfer object)
+    :type transistor_dto: dct.TransistorDTO
+    :return: transistor conduction loss in W
+    :rtype: float
+    """
+    return transistor_dto.r_channel * transistor_rms_current ** 2
