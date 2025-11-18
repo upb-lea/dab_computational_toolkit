2520fb49b24d0d7bb5211eab2d01ceaeb735bcbd
SevenOfNinePE <Andreas.Sack@uni-siegen.de>
2025-11-18 16:32:05 +0100

    Move and rename files to structure designed for multi topologies.

---
+++ dct/topology/__init__.py
@@ -0,0 +1,7 @@
+
+"""Make all topologies visible."""
+from . import dab
+
+__all__ = ["dab"]
+
+# For additional topology: __all__ = ["dab", "[new topology]"]
---
+++ dct/topology/dab/__init__.py
@@ -0,0 +1,11 @@
+
+from .dab_circuit_topology import *
+from .dab_currents import *
+from .dab_mod_zvs import *
+from .dab_plotdab import *
+from .dab_plot_window import *
+from .dab_plot_calculation import *
+from .dab_plot_simulation import *
+from .dab_plot_waveforms import *
+from .dab_generalplotsettings import *
+from .dab_functions_waveforms import *
