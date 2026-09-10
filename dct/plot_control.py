1922009633f65061e5f2ea1fa5125b41518456aa
gituser789 <62549000+gituser789@users.noreply.github.com>
2026-09-10 21:01:47 +0200

    adapt scale for efficiency

--- dct/plot_control.py
+++ dct/plot_control.py
@@ -532,7 +532,7 @@ class ParetoPlots:
         x_scale_max = 1.1 * df_filtered[total_volume_key].max() * FACTOR_M3_TO_CM3
 
         y_scale_min = 0.95 * df_filtered[total_mean_loss_key].min()
-        y_scale_max = 1.05 * df_filtered[total_mean_loss_key].max()
+        y_scale_max = np.clip(1.05 * df_filtered[total_mean_loss_key].max(), y_scale_min, 1)
 
         # add Color list
         if not is_summary:
